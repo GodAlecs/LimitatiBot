08abdb0b05f01520515d8502fcfa1c0aa0c53fae
Schifo-Dev-69 <97362673+Schifo-Dev-69@users.noreply.github.com>
2022-02-03 00:02:10 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,79 +5,61 @@
 # The sale of the SRC CODE is prohibited
 
 # Libraries
+import asyncio
+import json
 from pyrogram import Client, filters
 from pyrogram.types import Message
-import json
 
 app = Client(
-    "main",
-    api_id = 0,  # Insert your telegram API ID before the comma, remember NOT TO PUT the ""
-    api_hash = "",  # Insert your HASH API into the ""
-    bot_token = ""  # Enter the bot token
+    'main',
+    api_id = 0,  # Insert your telegram API ID before the comma, remember NOT TO PUT the ''
+    api_hash = '',  # Insert your HASH API into the ''
+    bot_token = ''  # Enter the bot token
 )
 
-ALIAS = ["/"]  # Command prefixes
-ADMIN = 1845164702  # ID ADMIN
-CMDB = ["start"]  # Commands not to be seen by admins
+ALIAS = ['/']  # Command prefixes
+ADMINS = [1845164702]  # ID ADMIN
+CMDB = ['start']  # Commands not to be seen by admins
 
-lingua = json.load(
-    open(f"lang/english.json")
-)  # <- To change the bot language, replace the language name, example from "english.json" to "italiano.json"
+# To change the bot language, replace the language name, example from 'english.json' to 'italiano.json'
+lingua = json.load(open(f'lang/english.json'))
 
 
-# The available languages ​​can be found in the "lang" directory in the bot's source code
+# The available languages ​​can be found in the 'lang' directory in the bot's source code
 
 # Start
 
-@app.on_message(filters.command("start", prefixes = ALIAS) & filters.private)
+@app.on_message(filters.command('start', prefixes = ALIAS) & filters.private)
 async def start(app: Client, message: Message):
-    await app.send_message(message.chat.id, lingua["start"])
+    await app.send_message(message.chat.id, lingua['start'])
 
 
 # Start Chat
 
-@app.on_message(filters.private & ~filters.me & ~filters.command(CMDB), group = -30)
-async def chat(app: Client, message: Message):
-    chatid = message.chat.id
-    if message.from_user.id != ADMIN:
-        messageSent = await message.reply_text(lingua['message_send_message'])
-        if message.text:
-            await app.send_message(
-                ADMIN,
-                f'{lingua["user_prefix"]} {message.from_user.mention} ➣ {message.text}'
-            )
-        else:
-            await app.forward_messages(
-                chat_id = ADMIN, from_chat_id = message.chat.id,
-                message_ids = message.message_id
-            )
-            await app.send_message(
-                ADMIN,
-                f'🗄 {message.from_user.mention} {lingua["photo_reply_message"]}'
-            )
+@app.on_message(filters.private & filters.user(ADMINS) & ~filters.command(CMDB) & filters.reply)
+async def from_admins(app: Client, message: Message):
+    await app.copy_message(
+        message.reply_to_message.entities[0].user.id,
+        message.chat.id,
+        message.message_id
+    )
 
-    # Admin Settings
 
-    else:
-        if message.reply_to_message:
-            if message.text:
-                r = message.reply_to_message.entities[0].user
-                await app.send_message(
-                    r.id,
-                    f'{lingua[f"operator_message_prefix"]} {message.text}'
-                )
-            else:
-                r = message.reply_to_message.entities[0].user
-                await app.copy_message(
-                    r.id,
-                    chatid,
-                    message.message_id
-                )
-        else:
-            await app.send_message(
-                chatid,
-                lingua["error_message_reply"]
-            )
+@app.on_message(filters.private & filters.user(ADMINS) & ~filters.command(CMDB))
+async def from_admins_not_reply(app: Client, message: Message):
+    await app.send_message(message.chat.id, lingua['error_message_reply'])
+
+
+@app.on_message(filters.private & ~filters.me & ~filters.user(ADMINS) & ~filters.command(CMDB))
+async def from_users(app: Client, message: Message):
+    await message.reply_text(lingua['message_send_message'])
+    for admin in ADMINS:
+        await asyncio.gather(
+            message.copy(admin),
+            app.send_message(admin, f'🗄 {message.from_user.mention} {lingua["photo_reply_message"]}'),
+            return_exceptions = True
+        )
+        await asyncio.sleep(0.5)
 
 
 app.run()
